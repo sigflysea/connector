5d1050143bde8e43b208a69b272c41f31d53de80
sigflysea <junfree@gmail.com>
2020-08-12 10:10:36 -0500

    add middleware auth, JWT

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
+const User = require('../../models/User');
 //@route   Get api/auth
 //@desc    Test route
 //@access  Public
-router.get("/", (req, res) => res.send("auth route"));
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.userP.id).select('-password');
+        res.json(user);
+        console.log(req);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 //@route   POST api/users
 //@desc    Test route
@@ -47,8 +49,15 @@ router.post(
 
             await user.save();
             //Return jasonwebtoken
-
-            res.send('User registered');
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+            jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 3600 }, (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
         } catch (err) {
             console.error(err);
             res.status(500).send('Server error');
