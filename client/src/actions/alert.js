cfa2e0d81941c04b06ea93b8340567d36e07ba89
sigflysea <junfree@gmail.com>
2020-09-09 12:02:08 -0500

    Auth reducer and Register action

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,8 @@
 import uuid from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-//were able to do this by Thunk middleware
+//were able to do this by Thunk middleware  destructing action to
+// msg and aleT???
 //adding timeout as time parameter with 5000 default value
 export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
     const id = uuid.v4();
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,11 +3,12 @@ import { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 //import axios from 'axios';
 
 //(props) destructuring  ({setAlert}) =>
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -25,7 +26,8 @@ const Register = ({ setAlert }) => {
             setAlert('Pass not match', 'danger');
             console.log('Pass not match');
         } else {
-            console.log(formData);
+            register({ name, email, password });
+            //console.log(formData);
             //This section axios replaced by Redux
             // const user = {
             //     name,
@@ -60,7 +62,7 @@ const Register = ({ setAlert }) => {
                         name='name'
                         value={name}
                         onChange={(e) => onChange(e)}
-                        required
+                        // required
                     />
                 </div>
                 <div className='form-group'>
@@ -70,7 +72,7 @@ const Register = ({ setAlert }) => {
                         name='email'
                         value={email}
                         onChange={(e) => onChange(e)}
-                        required
+                        // required
                     />
                     <small className='form-text'>
                         This site uses Gravatar so if you want a profile image,
@@ -114,5 +116,6 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 };
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
