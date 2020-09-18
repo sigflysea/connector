37f5ed1adbb08d10c35a3faf124a452cba31a058
sigflysea <junfree@gmail.com>
2020-09-18 11:25:56 -0500

    two <li> in auth link, no error

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -48,21 +48,32 @@ const Navbar = ({ auth: { isAuthenticated }, logout }) => {
                     <i className='fas fa-code'></i> DevConnector
                 </Link>
             </h1>
-
-            <Fragment>{isAuthenticated ? authlinks : guestLinks}</Fragment>
-
-            {/* // (isAuthenticated ? (
-                //     <li>
-                //         <a onClick={logout} href='#!'>
-                //             <i className='fas fa-sign-out-alt'></i>{' '}
-                //             <span className='hide-sm'>Logout</span>
-                //         </a>
-                //     </li>
-                // ) : (
-                //     <li>
-                //         <Link to='/login'>Login</Link>
-                //     </li>
-                // )) */}
+            {/* <Fragment>{isAuthenticated ? authlinks : guestLinks}</Fragment> */}
+            {isAuthenticated ? (
+                <ul>
+                    <li>
+                        <Link to='/profiles'>Developers</Link>
+                    </li>
+                    <li>
+                        <a onClick={logout} href='#!'>
+                            <i className='fas fa-sign-out-alt'></i>{' '}
+                            <span className='hide-sm'>Logout</span>
+                        </a>
+                    </li>
+                </ul>
+            ) : (
+                <ul>
+                    <li>
+                        <a href='#!'>Developers</a>
+                    </li>
+                    <li>
+                        <Link to='/register'>Register</Link>
+                    </li>
+                    <li>
+                        <Link to='/login'>Login</Link>
+                    </li>
+                </ul>
+            )}
         </nav>
     );
 };
