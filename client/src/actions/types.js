43e4e7beb41c7ea0c84517fcd10b5ff1da4d8491
sigflysea <junfree@gmail.com>
2020-09-22 11:10:45 -0500

    Dashboard PrivateRoute add

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,3 +7,5 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_ERROR = 'LOGIN_ERROR';
 export const LOGOUT = 'LOGOUT';
+export const GET_PROFILE = 'GET_PROFILE';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
