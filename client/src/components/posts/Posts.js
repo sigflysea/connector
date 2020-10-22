1736c929d84493d3e569a2c0f10269ddbae195a5
sigflysea <junfree@gmail.com>
2020-10-22 13:48:47 -0500

    Add Like and unLike

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,14 +3,26 @@ import PropTypes from 'prop-types';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
 import { connect } from 'react-redux';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
     useEffect(() => {
         getPosts();
     }, [getPosts]);
-    return (
+    return loading ? (
+        <Spinner></Spinner>
+    ) : (
         <Fragment>
-            <Spinner></Spinner>
+            <h1 className='large text-primary'>Posts</h1>
+            <p className='lead'>
+                <i className='fas fa-user' /> Welcome to the community
+            </p>
+
+            <div className='posts'>
+                {posts.map((post) => (
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
         </Fragment>
     );
 };
