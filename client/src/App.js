74f786a191545ec4810e77efa56638303019ed98
sigflysea <junfree@gmail.com>
2020-11-02 15:47:08 -0600

    finished Routes for NotFound

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Alert from './components/layout/Alert';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/profile-forms/CreateProfile';
-import EditProfile from './components/profile-forms/EditProfile';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import AddExperience from './components/profile-forms/AddExperience';
-import AddEducation from './components/profile-forms/AddEducation';
-import PrivateRoute from './components/routing/PrivateRoute';
-import Posts from './components/posts/Posts';
-import Post from './components/post/Post';
+import Routes from './components/routing/Routes';
 import './App.css';
 //Redux
 import { Provider } from 'react-redux';
@@ -32,49 +20,13 @@ function App() {
     return (
         <Provider store={store}>
             <Router>
-                <div>
+                <Fragment>
                     <Navbar />
-                    <Route exact path='/' component={Landing} />
-                    <section className='container'>
-                        <Alert />
-                        <Switch>
-                            <Route path='/login' component={Login} />
-                            <Route path='/register' component={Register} />
-                            <Route path='/profiles' component={Profiles} />
-                            <Route path='/profile/:id' component={Profile} />
-                            <PrivateRoute
-                                path='/dashboard'
-                                component={Dashboard}
-                            />
-                            <PrivateRoute
-                                path='/create-profile'
-                                component={CreateProfile}
-                            />
-                            <PrivateRoute
-                                path='/edit-profile'
-                                component={EditProfile}
-                            />
-                            <PrivateRoute
-                                path='/add-experience'
-                                component={AddExperience}
-                            />
-                            <PrivateRoute
-                                path='/add-education'
-                                component={AddEducation}
-                            />
-                            <PrivateRoute
-                                exact
-                                path='/posts'
-                                component={Posts}
-                            />
-                            <PrivateRoute
-                                exact
-                                path='/posts/:id'
-                                component={Post}
-                            />
-                        </Switch>
-                    </section>
-                </div>
+                    <Switch>
+                        <Route exact path='/' component={Landing} />
+                        <Route component={Routes} />
+                    </Switch>
+                </Fragment>
             </Router>
         </Provider>
     );
---
+++ client/src/components/routing/Routes.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Alert from '../layout/Alert';
+import Login from '../auth/Login';
+import Register from '../auth/Register';
+import Dashboard from '../dashboard/Dashboard';
+import CreateProfile from '../profile-forms/CreateProfile';
+import EditProfile from '../profile-forms/EditProfile';
+import Profiles from '../profiles/Profiles';
+import Profile from '../profile/Profile';
+import AddExperience from '../profile-forms/AddExperience';
+import AddEducation from '../profile-forms/AddEducation';
+import PrivateRoute from './PrivateRoute';
+import Posts from '../posts/Posts';
+import Post from '../post/Post';
+import NotFound from '../layout/NotFound';
+
+const Routes = () => {
+    return (
+        <section className='container'>
+            <Alert />
+            <Switch>
+                <Route path='/login' component={Login} />
+                <Route path='/register' component={Register} />
+                <Route path='/profiles' component={Profiles} />
+                <Route path='/profile/:id' component={Profile} />
+                <Route component={NotFound} />
+                <PrivateRoute path='/dashboard' component={Dashboard} />
+                <PrivateRoute
+                    path='/create-profile'
+                    component={CreateProfile}
+                />
+                <PrivateRoute path='/edit-profile' component={EditProfile} />
+                <PrivateRoute
+                    path='/add-experience'
+                    component={AddExperience}
+                />
+                <PrivateRoute path='/add-education' component={AddEducation} />
+                <PrivateRoute exact path='/posts' component={Posts} />
+                <PrivateRoute exact path='/posts/:id' component={Post} />
+            </Switch>
+        </section>
+    );
+};
+
+export default Routes;
